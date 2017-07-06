e9ccdbf6e15191010ac8b0840d50f732b5a485c0
Ariel Werle <ariel@astro.ufsc.br>
2017-07-06 18:42:55 -0300

    Added functions to plot filter curves and residual spectra

--- starlight_toolkit/plotting.py
+++ starlight_toolkit/plotting.py
@@ -11,12 +11,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 from starlight_toolkit.tables import read_output_table
 
+
 def plot_spec(out_file, ax=plt.subplot(111), plot_obs=True, plot_error=False
 , plot_labels=True, obs_color='k', syn_color='b', w0_color='y'
 , syn_label='Fitted Spectrum'):
     '''
     TODO: Add marks for clipped wavelengths, documentation.
-
     '''
 
     out = read_output_table(out_file)
@@ -30,8 +30,8 @@ def plot_spec(out_file, ax=plt.subplot(111), plot_obs=True, plot_error=False
 
     if plot_obs==True:
 
-        f_obs_masked = np.ma.masked_array(data = f_obs, mask = w0)
-        f_w0         = np.ma.masked_array(data = f_obs, mask = ~w0)
+        f_obs_masked = np.ma.masked_array(data=f_obs, mask=w0)
+        f_w0         = np.ma.masked_array(data=f_obs, mask=~w0)
 
         ax.plot(l_obs, f_obs, color=obs_color, lw=0.5, label='Observed Spectrum')
         ax.plot(l_obs, f_w0, color=w0_color, lw=0.5, label=r'$w_\lambda=0$')
@@ -47,3 +47,41 @@ def plot_spec(out_file, ax=plt.subplot(111), plot_obs=True, plot_error=False
 
     ax.set_ylim(0, 1.2 * np.max(f_syn))
     ax.set_xlim(out['keywords']['l_ini'],out['keywords']['l_fin'])
+
+
+def plot_filter(filter_file, ax=plt.subplot(111), filter_color='k'
+, filter_ls='dashed', redshift=0, scale_factor=1):
+    '''
+    Plots filter file.
+
+    To plot filters in the restframe, you should provide the galaxy's redshift,
+    which is set to zero as default.
+
+    The variable scale_factor will be multiplied by the filter's transmission.
+
+    '''
+    #Reading filter:
+    wl_filter, T_filter = np.genfromtxt(filter_file).transpose()
+
+    #Plotting:
+    ax.plot(wl_filter/(1+redshift), T_filter*scale_factor, color=filter_color
+    , linestyle=filter_ls)
+
+
+def plot_residual_spec(out_file, ax=plt.subplot(111), residual_color='k'
+, plot_labels=True):
+
+    out = read_output_table(out_file)
+
+    l_obs, f_obs, f_syn, f_wei = out['spectra']['l_obs'], \
+    out['spectra']['f_obs'], out['spectra']['f_syn'], out['spectra']['f_wei']
+
+    w0 = out['spectra']['f_wei'] <= -1
+
+    residual = np.ma.masked_array(data=(f_obs-f_syn)/f_syn, mask=w0)
+
+    ax.plot(l_obs, residual, lw=0.25, color=residual_color)
+
+    if plot_labels==True:
+        ax.set_xlabel(r'$\lambda[\mathrm{\AA}]$')
+        ax.set_ylabel(r'Residual Spectrum')
