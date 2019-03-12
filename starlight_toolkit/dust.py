efe0e2857fd13404c935c351050d564e76b213bb
Ariel Werle <ariel.werle@gmail.com>
2019-03-12 16:18:20 -0300

    fixed bug

--- starlight_toolkit/dust.py
+++ starlight_toolkit/dust.py
@@ -10,15 +10,32 @@ import numpy as np
 
 
 
-def calc_extinction(lamb, EBV, Rv=3.1):
-    #Calculate AV and A_lambda:
+def calc_extinction(wl, EBV, Rv=3.1):
+    """
+    Calculates galactic extinction at a given wavelength
+
+    Parameters
+    ----------
+
+    wl: array like
+        Wavelengths in angstroms
+    Rv (optional): float
+        Ratio of total to selective extinction, default is the average MW value of 3.1
+
+    Returns
+    ----------
+    A_lambda: array
+        Galactic extinction at wl
+
+    """
+
     AV = Rv * EBV
-    A_lambda = AV * CCM(lamb)
+    A_lambda = AV * CCM(wl)
 
     return A_lambda
 
 
-def extinction_corr(spectra,lambdas,EBV):
+def extinction_corr(flux, wl, EBV):
     '''
 
     Corrects spectra for the effects of galactic extinction.
@@ -29,15 +46,15 @@ def extinction_corr(spectra,lambdas,EBV):
     '''
 
     #Get the extinction in each wavelenght:
-    A_lambdas = calc_extinction(lambdas,EBV)
+    A_lambdas = calc_extinction(wl, EBV)
 
     #Calculate corrected spectra:
-    corr_spectra = spectra * 10 ** (0.4 * A_lambdas)
+    corr_spectra = flux * 10 ** (0.4 * A_lambdas)
 
     return corr_spectra
 
 
-def extinction_decorr(spectra,lambdas,EBV):
+def extinction_decorr(wl, flux, EBV):
     '''
 
     Input: Wavelenghts, Fluxes, RA, Dec
@@ -46,39 +63,47 @@ def extinction_decorr(spectra,lambdas,EBV):
     '''
 
     #Get the extinction in each wavelenght:
-    A_lambdas = calc_extinction(lambdas,EBV)
+    A_lambda = calc_extinction(wl, EBV)
 
     #Calculate corrected spectra:
-    corr_spectra = spectra * 10 ** (-0.4 * A_lambdas)
+    corr_spectra = flux * 10 ** (-0.4 * A_lambda)
 
     return corr_spectra
 
 
-def CCM(l, R_V=3.1):
-    '''
-
+def CCM(wl, R_V=3.1):
+    """
     Calculates the Cardelli, Clayton & Mathis (CCM) extinction curve.
 
-    ---
-        Input:   Wavelengths, Rv (Optional, default is 3.1)
-        Returns: A_lambda/AV
+    Parameters
+    ----------
+    wl : array like
+        Wavelength in Angstroms.
+
+    R_V : float, optional
+        Ratio of total to selective extinction.
+
+    Returns
+    -------
+    q : array
+        Extinction A_lambda / A_V. Array of same length as wl.
 
 
     Reference: http://adsabs.harvard.edu/abs/1989ApJ...345..245C
 
-    '''
+    """
 
 
-    a = np.zeros(np.shape(l))
-    b = np.zeros(np.shape(l))
-    F_a = np.zeros(np.shape(l))
-    F_b = np.zeros(np.shape(l))
-    x = np.zeros(np.shape(l))
-    y = np.zeros(np.shape(l))
-    q = np.zeros(np.shape(l))
+    a = np.zeros(np.shape(wl))
+    b = np.zeros(np.shape(wl))
+    F_a = np.zeros(np.shape(wl))
+    F_b = np.zeros(np.shape(wl))
+    x = np.zeros(np.shape(wl))
+    y = np.zeros(np.shape(wl))
+    q = np.zeros(np.shape(wl))
 
-    x = 10000. / l
-    y = 10000. / l - 1.82
+    x = 10000. / wl
+    y = 10000. / wl - 1.82
 
     # Far-Ultraviolet: 8 <= x <= 10 ; 1000 -> 1250 Angs
     i = np.bitwise_and(x >= 8, x <= 10)
@@ -116,60 +141,57 @@ def CCM(l, R_V=3.1):
     return q
 
 
-def CAL(l, R_V=4.05):
-    '''
-    Calculates the reddening law by Calzetti et al. (1994).
-
-    q = A_lambda / A_V for Calzetti et al reddening law (formula from hyperz-manual).
-    l = lambda, in Angstrons
-    x = 1 / lambda in 1/microns
+def CAL(wl, R_V=4.05):
+    """
+    Calculates the attenuation curve by Calzetti et al. (1994).
 
     Parameters
     ----------
-    l : array like
+    wl : array like
         Wavelength in Angstroms.
 
     R_V : float, optional
-        Selective extinction parameter (roughly "slope").
+        Ratio of total to selective attenuation.
 
     Returns
     -------
     q : array
-        Extinction A_lamda / A_V. Array of same length as l.
+        Attenuation A_lambda / A_V. Array of same length as wl.
 
-    '''
-    if not isinstance(l, np.ma.MaskedArray):
-        l = np.asarray(l, 'float64')
+    """
 
-    x = 1e4 / l
-    q = np.zeros_like(l)
+    if not isinstance(wl, np.ma.MaskedArray):
+        wl = np.asarray(wl, 'float64')
+
+    x = 1e4 / wl
+    q = np.zeros_like(wl)
 
     # UV -> Optical: 1200 -> 6300 Angs
-    i = (l >= 1200.) & (l <= 6300.)
+    i = (wl >= 1200.) & (wl <= 6300.)
     q[i] = (2.659 / R_V) * (-2.156 + 1.509 * x[i] - 0.198 * x[i]**2 + 0.011 * x[i]**3) + 1.
 
     # Red -> Infrared
-    i = (l >= 6300.) & (l <= 22000.)
+    i = (wl >= 6300.) & (wl <= 25000.)
     q[i] = (2.659 / R_V) * (-1.857 + 1.040 * x[i]) + 1.
 
     # Issue a warning if lambda falls outside 1200->22000 Angs range
-    if ((l < 1200.) | (l > 22000.)).any():
+    if ((wl < 1200.) | (wl > 25000.)).any():
         print('[Calzetti_RedLaw] WARNING! some lambda outside valid range (1200, 22000.)')
     return q
 
 
 
-def CSB(l, R_V=3.1, B=0.5):
+def CSB(wl, R_V=3.1, B=0.5):
 
-    a   = np.zeros(np.shape(l))
-    b   = np.zeros(np.shape(l))
-    F_a = np.zeros(np.shape(l))
-    F_b = np.zeros(np.shape(l))
-    x   = np.zeros(np.shape(l))
-    y   = np.zeros(np.shape(l))
-    q   = np.zeros(np.shape(l))
+    a   = np.zeros(np.shape(wl))
+    b   = np.zeros(np.shape(wl))
+    F_a = np.zeros(np.shape(wl))
+    F_b = np.zeros(np.shape(wl))
+    x   = np.zeros(np.shape(wl))
+    y   = np.zeros(np.shape(wl))
+    q   = np.zeros(np.shape(wl))
 
-    x = 10000. / l
+    x = 10000. / wl
     y = x - 1.82
 
     # Far-Ultraviolet: 8 <= x <= 10 ; 1000 -> 1250 Angs
@@ -218,33 +240,33 @@ def CSB(l, R_V=3.1, B=0.5):
     return q
 
 
-def CCC(l, R_V=4.05):
+def CCC(wl, R_V=4.05):
 
-    q = np.zeros(np.shape(l))
-    x = 10000. / l
+    q = np.zeros(np.shape(wl))
+    x = 10000. / wl
 
-    i_CCC = np.bitwise_and(l>=912, l<1846)
+    i_CCC = np.bitwise_and(wl>=912, wl<1846)
     q[i_CCC] = (5.472 + 0.671 * x[i_CCC] - 9.218e-3 * x[i_CCC]**2 + 2.620e-3 * x[i_CCC]**3)/R_V
 
-    i_CAL = np.where(l>=1846)
-    q[i_CAL] = CAL(l[i_CAL], R_V)
+    i_CAL = np.where(wl>=1846)
+    q[i_CAL] = CAL(wl[i_CAL], R_V)
 
     return q
 
 
-def Salim2018(l, delta=0, B=0, gamma=350., l_bump=2175.):
+def Salim2018(wl, delta=0, B=0, gamma=350., wl_bump=2175.):
     '''
     Generates the modified Calzetti law from Salim et al. (2018), which
     is itself a modification of the Noll et al. (2009) and Kriek & Conroy (2011)
     parameterizations.
     '''
 
-    q = np.zeros(np.shape(l))
+    q = np.zeros(np.shape(wl))
 
     #Generate Drude profile:
-    D = B * (l**2) * (gamma**2) / ( (l**2-l_bump**2)**2 + (l**2 * gamma**2))
+    D = B * (wl**2) * (gamma**2) / ( (wl**2-wl_bump**2)**2 + (wl**2 * gamma**2))
 
     #Calculate q_lambda
-    q = CCC(l) * (l/5500)**delta + D
+    q = CCC(wl) * (wl/5500)**delta + D
 
     return q
