4b2ecbaf906491d578409aed139d3dc1e9ef86e5
Ariel Werle <ariel.werle@gmail.com>
2019-07-01 21:11:37 -0300

    Minor fixes

--- starlight_toolkit/synphot.py
+++ starlight_toolkit/synphot.py
@@ -23,28 +23,34 @@ def resampler(x_old, y_old, x_new):
     return y_new
     
 
-def synflux(wl, flux, filter_file):
+def synflux(wl, flux, filter_curve):
     
-    # Reading filter:
-    wl_filter, T_filter = np.genfromtxt(filter_file).transpose()
+    # Reading filter if needed:
+    if type(filter_curve) is str: 
+        wl_filter, T = np.genfromtxt(filter_curve).transpose()
+    else:
+        wl_filter, T = filter_curve[0], filter_curve[1]
     
     # Resampling filter and spectrum to 1\AA intervals:
-    wl_filter_new = np.arange(np.round(wl_filter[0])-5, np.round(wl_filter[-1])+5)
+    wl_new = np.arange(np.round(wl_filter[0])-5, np.round(wl_filter[-1])+5)
     
-    T_filter_new = resampler(wl_filter, T_filter, wl_filter_new)
-    flux_new = resampler(wl, spectrum, wl_filter_new)
+    T = resampler(wl_filter, T, wl_new)
+    flux = resampler(wl, flux, wl_new)
     
     # Convolution:
-    synflux = np.trapz(flux_new * T_filter_new * wl_filter_new, dx=1)
-    synflux /= np.trapz(wl_filter_new * T_filter_new, dx=1)
+    synflux = np.trapz(flux * T * wl_new, dx=1)
+    synflux /= np.trapz(wl_new * T, dx=1)
 
     return synflux
 
 
-def synmag(wl, flux, error, flag, filter_file, badpix_tolerance=0.25):
+def synmag(wl, flux, error, flag, filter_curve, badpix_tolerance=0.25, interpolate_bad_pixels=False):
 
-    # Reading filter:
-    wl_filter, T = np.genfromtxt(filter_file).transpose()
+    # Reading filter if needed:
+    if type(filter_curve) is str: 
+        wl_filter, T = np.genfromtxt(filter_file).transpose()
+    else:
+        wl_filter, T = filter_curve[0], filter_curve[1]
 
     # Checking bad pixels:
     filter_range = (wl > wl_filter[0]) & (wl < wl_filter[-1])
@@ -53,48 +59,52 @@ def synmag(wl, flux, error, flag, filter_file, badpix_tolerance=0.25):
     else:
         badpix = False
 
-    # Resampling filter and spectrum to 1\AA intervals:
+    # Resampling filter and spectrum to 1 Angstrom intervals:
     wl_new = np.arange(np.round(wl_filter[0]) - 5, np.round(wl_filter[-1]) + 5)
 
-    T = resampler(wl_filter, T, wl_new)
-    flux = resampler(wl[~flag], flux[~flag], wl_new) # Excluding bad pixels
-    error = resampler(wl[~flag], error[~flag], wl_new) # Excluding bad pixels
+    # Projecting fluxes in the filter region, excluding or including bad pixels:
+    if interpolate_bad_pixels is True:
+        T = resampler(wl_filter, T, wl_new)
+        flux = resampler(wl[~flag], flux[~flag], wl_new) # Excluding bad pixels
+        error = resampler(wl[~flag], error[~flag], wl_new) # Excluding bad pixels
+    else: 
+        T = resampler(wl_filter, T, wl_new)
+        flux = resampler(wl, flux, wl_new) # Including bad pixels
+        error = resampler(wl, error, wl_new) # Including bad pixels
 
-    # Convolution:
+    # Calculate magnitudes and errors:
     m_ab = -2.5 * np.log10(np.trapz(flux * T * wl_new, dx=1) / np.trapz(T / wl_new, dx=1)) - 2.41
-
-    m_ab_error = 1.0857362047581294 * np.sqrt(np.sum(T**2 * error**2 * wl_new ** 2)) / np.sum(flux * T * wl_new)
+    m_ab_error = 1.0857 * np.sqrt(np.sum(T**2 * error**2 * wl_new ** 2)) / np.sum(flux * T * wl_new)
 
     return m_ab, m_ab_error, badpix
 
 
+def pivot_wavelength(filter_curve):
 
-
-def pivot_wavelength(filter_file):
-
-    # Reading and resampling filter:
-    wl_filter, T_filter = np.genfromtxt(filter_file).transpose()    
+    # Reading filter if needed:
+    if type(filter_curve) is str: 
+        wl_filter, T = np.genfromtxt(filter_file).transpose()
+    else:
+        wl_filter, T = filter_curve[0], filter_curve[1]
     
-    wl_filter_new = np.arange(np.round(wl_filter[0])-5, np.round(wl_filter[-1])+5, 1)
-    
-    T_filter_new = resampler(wl_filter, T_filter, wl_filter_new)
-    
-    # Calculating pivot_wavelength
-    
-    pivot_wl = np.trapz(T_filter_new * wl_filter_new, dx=1) / np.trapz(T_filter_new * (wl_filter_new**-1), dx=1)
+    # Calculating pivot_wavelength    
+    pivot_wl = np.trapz(T * wl_filter, dx=1) / np.trapz(T * (wl_filter**-1), dx=1)
     pivot_wl = np.sqrt(pivot_wl)
     
     return pivot_wl
 
 
-def effective_wavelength(wl, spectrum, filter_file):
+def effective_wavelength(wl, spectrum, filter_curve):
     '''
     This is defined as the mean wavelength of the filter weighted by transmission of the filter
     and spectrum of the source
     '''
     
-    # Reading filter:
-    wl_filter, T_filter = np.genfromtxt(filter_file).transpose()
+    # Reading filter if needed:
+    if type(filter_curve) is str: 
+        wl_filter, T = np.genfromtxt(filter_file).transpose()
+    else:
+        wl_filter, T = filter_curve[0], filter_curve[1]
     
     # Resampling filter and spectrum to 1\AA intervals:
     wl_filter_new = np.arange(np.round(wl_filter[0])-5, np.round(wl_filter[-1])+5,1)
